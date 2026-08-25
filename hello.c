2c12874c09c28b08d49af9fa477e8425f6d47dfb
Cobraa315 <34480100@nebraska.edu>
2026-08-25 10:23:09 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Chase Tarwater
+ *         ctarwater2@huskers.unl.edu
+ * Date: 2026/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("I love the School of Computing!\n");
+
+  return 0;
+}
